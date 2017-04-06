e69b82fb7429cf0af9ecec6dd8651bb6eeb3ccdc
gwo <g.wolf@online.de>
2017-04-06 09:30:46 +0200

    Initial commit

---
+++ src/test/java/com/teamtreehouse/web/controller/WeatherControllerTest.java
@@ -0,0 +1,11 @@
+package com.teamtreehouse.web.controller;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+public class WeatherControllerTest {
+    private MockMvc mockMvc;
+    private WeatherController controller;
+    
+    
+
+}
